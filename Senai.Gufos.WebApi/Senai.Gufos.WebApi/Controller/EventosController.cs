6b6a0a42bbe6869fce3fc7bf4c76d6d832e0f4bd
Lucas Melegaro De Oliveira <47218236812@info.local>
2019-08-26 16:30:41 -0300

    Up

---
+++ Senai.Gufos.WebApi/Senai.Gufos.WebApi/Controller/EventosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Gufos.WebApi.Domains;
+using Senai.Gufos.WebApi.Repositories;
+
+namespace Senai.Gufos.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class EventosController : ControllerBase
+    {
+        // repositorio
+        EventoRepository EventoRepository = new EventoRepository();
+        // endpoints
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(EventoRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Eventos evento)
+        {
+            try
+            {
+                EventoRepository.Cadastrar(evento);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new {  mensagem = "Erro ao cadastrar." + 
+                    ex.Message });
+            }
+
+        }
+    }
+}
