8d0a36f2376b6c7d67f8eb1fc3693e5cc5f35ad7
Lucas Melegaro De Oliveira <47218236812@info.local>
2019-08-26 17:31:13 -0300

    Up

---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Controller/EstiloController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.Repositories;
+
+namespace Senai.Optus.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstiloController : ControllerBase
+    {
+        EstiloRepository EstiloRepository = new EstiloRepository();
+
+        [HttpGet]
+        // IEnumerable<Estilos>
+        public IActionResult Listar()
+        {
+            return Ok(EstiloRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Estilos estilos)
+        {
+            try
+            {
+                EstiloRepository.Cadastrar(estilos);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Eita, erro:" + ex.Message });
+            }
+        }
+
+
+    }
+}
---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,72 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                // facilitar a nossa vida
+                // select 
+                return ctx.Estilos.ToList();
+            }
+        }
+
+        public void Cadastrar(Estilos estilos)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                // insert into eventos (nome) values (@nome)
+                ctx.Estilos.Add(estilos);
+                ctx.SaveChanges();
+            }
+        }
+
+        // buscar por id 
+        public Estilos BuscarPorId(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.FirstOrDefault(x => x.IdEstilo == id);
+            }
+        }
+
+        // atualizar
+        public void Atualizar(Estilos estilos)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Estilos EstiloBuscado = ctx.Estilos.FirstOrDefault(x => x.IdEstilo == estilos.IdEstilo);
+                // update estilos set nome = @nome
+                EstiloBuscado.Nome = estilos.Nome;
+                // insert - add, delete - remove, update - update
+                ctx.Estilos.Update(EstiloBuscado);
+                // efetivar
+                ctx.SaveChanges();
+            }
+        }
+
+        // deletar
+        public void Deletar(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                // encontrar o item
+                // chave primaria da tabela
+                Estilos estilos = ctx.Estilos.Find(id);
+                // remover do contexto
+                ctx.Estilos.Remove(estilos);
+                // efetivar as mudanças no banco de dados
+                ctx.SaveChanges();
+
+                //
+            }
+        }
+    }
+}
